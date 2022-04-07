bccc1b67fdcc749710ed5ea8c5c3e72351cf2693
johnsqliu <johnsqliu@tencent.com>
2022-04-07 15:18:29 +0800

    update

--- vue.config.js
+++ vue.config.js
@@ -1,17 +1,17 @@
 const { defineConfig } = require("@vue/cli-service");
 
-const { name } = require('./package');
+const { name } = require("./package");
 
 module.exports = defineConfig({
   devServer: {
     headers: {
-      'Access-Control-Allow-Origin': '*',
+      "Access-Control-Allow-Origin": "*",
     },
   },
   configureWebpack: {
     output: {
       library: `${name}-[name]`,
-      libraryTarget: 'umd', // 把微应用打包成 umd 库格式
+      libraryTarget: "umd", // 把微应用打包成 umd 库格式
       chunkLoadingGlobal: `webpackJsonp_${name}`,
     },
   },
